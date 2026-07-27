3f2aa5dc004987623093898e7922734abf233005
Evan Mulumba <evan.mulumba@almaviacx.com>
2026-07-27 18:49:08 +0200

    add dotenv

--- src/main/java/com/endgame/endgameapi/EndgameApiApplication.java
+++ src/main/java/com/endgame/endgameapi/EndgameApiApplication.java
@@ -1,5 +1,6 @@
 package com.endgame.endgameapi;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EndgameApiApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
         SpringApplication.run(EndgameApiApplication.class, args);
     }
 
