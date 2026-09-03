605b34c4877d80ff61207f5ddcf5494c7672fcd2
Evan Mulumba <evan.mulumba@almaviacx.com>
2026-09-03 19:02:31 +0200

    UserGameService - remove unused var & methods

--- src/main/java/com/endgame/endgameapi/service/UserGameService.java
+++ src/main/java/com/endgame/endgameapi/service/UserGameService.java
@@ -3,9 +3,7 @@ package com.endgame.endgameapi.service;
 import com.endgame.endgameapi.dto.UserGameDTO;
 import com.endgame.endgameapi.model.GameStatus;
 import com.endgame.endgameapi.model.UserGame;
-import com.endgame.endgameapi.repository.GameRepository;
 import com.endgame.endgameapi.repository.UserGameRepository;
-import com.endgame.endgameapi.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
@@ -20,17 +18,10 @@ public class UserGameService {
 
     private final UserGameRepository userGameRepository;
 
-    public UserGameService(UserGameRepository userGameRepository, UserRepository userRepository, GameRepository gameRepository) {
+    public UserGameService(UserGameRepository userGameRepository) {
         this.userGameRepository = userGameRepository;
     }
 
-    public List<UserGame> getAllUserGames() {
-        return userGameRepository.findAll();
-    }
-
-    public UserGame saveUserGame(UserGame userGame) {
-        return userGameRepository.save(userGame);
-    }
 
     public boolean removeUserGame(Long id) {
         if (userGameRepository.existsById(id)) {
